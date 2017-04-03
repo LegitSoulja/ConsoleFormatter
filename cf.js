599719a51bc4eff6178448e6c0d9f96b003749e6
LSoulja <support@legitsoulja.info>
2017-04-02 23:42:57 -0400

    much more cleaner

--- cf.js
+++ cf.js
@@ -1,30 +1,33 @@
 (()=>{
-  // future use to make everything much easier, but got to make it :)
-  class printObject{
-      constructor(){
-      }
-  }
   Array.prototype.print = function() {
-      var c = [],d = [],a;
-      for (a in this)
-          if (this[a].msg && this[a])
-              if (this[a].color) {
-                  c.push("%c ", this[a].msg);
-                  var b = "color:" + this[a].color;
-                  this[a].background && (b += ";background-color:" + this[a].background);
-                  this[a].size && (b += ";font-size:" + this[a].size);
-                  this[a].fweight && (b += ";font-weight:" + this[a].fweight);
-                  this[a].border && (b += ";border:" + this[a].border);
-                  this[a].padding && (b += ";padding:" + this[a].padding);
-                  this[a].corner && (b += ";border-radius:" + this[a].corner);
-                  d.push(b)
-              } else c.push("%c " + this[a].msg),
-                  d.push("color:black");
-      b = c.join("");
-      c = [];
-      c.push(b);
-      for (a in d) c.push(d[a]);
-      c.splice(-1, 1);
-      console.log.apply(this, c)
+    var c = [], d = [];
+    for (let a in this) {
+      if (this[a].msg && this[a]) {
+        if (1 < Object.keys(this[a]).length) {
+          c.push("%c ", this[a].msg);
+          var b = "";
+          this[a].color && (b += `color:${this[a].color};`);
+          this[a].background && (b += `background-color:${this[a].background} ;`);
+          this[a].size && (b += `font-size:${this[a].size};`);
+          this[a].fweight && (b += `font-weight:${this[a].fweight};`);
+          this[a].font && (b += `font-family:${this[a].font}`);
+          this[a].border && (b += `border:${this[a].border};`);
+          this[a].lborder && (b += `border-left:${this[a].lborder};`);
+          this[a].align && (b += `text-align:${this[a].align};`);
+          this[a].rborder && (b += `border-right:${this[a].rborder};`);
+          this[a].tborder && (b += `border-top:${this[a].tborder};`);
+          this[a].bborder && (b += `border-bottom:${this[a].bborder};`);
+          this[a].bwidth && (b += `border-width:${this[a].bwidth};`);
+          this[a].padding && (b += `padding:${this[a].padding};`);
+          this[a].corner && (b += `border-radius:${this[a].corner};`);
+          d.push(b);
+        } else {
+          c.push("%c " + this[a].msg), d.push("color:black");
+        }
+      }
+    }
+    c = [c.join("")];
+    for (a in d) "string" == typeof d[a] && c.push(d[a]);
+    console.log.apply(this, c);
   };
 })();
